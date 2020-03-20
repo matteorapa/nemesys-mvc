5cd3220d83e6227a504272a46dd205f04f670bd6
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2020-03-20 15:01:20 +0100

    EFCore, EFCoreTools and EFCore SQL Server adapter installed

---
+++ mvc/Models/Investigation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Models
+{
+    public class Investigation
+    {
+        public int Id { get; set; }
+        public DateTime DateOfReport { get; set; }
+        public string Location { get; set; }
+        public DateTime HazardDate { get; set; }
+        public string ImageUrl { get; set; }
+        public int Upvotes { get; set; }
+    }
+}
