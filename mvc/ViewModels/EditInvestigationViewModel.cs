d5e0b0f6554bf8447f033b6cc7e87a311fef1e69
mikesciclunacalleja <michael.scicluna.calleja@gmail.com>
2020-04-13 17:24:59 +0200

    edit investigation view and investigation viewModel

--- mvc/ViewModels/EditInvestigationViewModel.cs
+++ mvc/ViewModels/EditInvestigationViewModel.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using mvc.ViewModels;
 
 namespace mvc.ViewModels
 {
     public class EditInvestigation
     {
+        [Required(ErrorMessage = "Please enter date of action.")]
+        public DateTime DateOfAction { get; set; }
+
+        [Required(ErrorMessage = "Please enter investigator's email")]
+        [StringLength(30, MinimumLength = 5)]
+        public string InvestigatorEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter investigator's phone number")]
+        public int InvestigatorPhone { get; set; }
+
+        [Required(ErrorMessage = "Please enter a brief investigation descritpion")]
+        [StringLength(100, MinimumLength = 1)]
+        public string InvDescription { get; set; }
     }
 }
