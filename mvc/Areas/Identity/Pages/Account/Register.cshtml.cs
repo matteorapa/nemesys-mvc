6becb25e4cf51f1d3998986b5954535266bb433c
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2020-06-01 17:03:03 +0200

    01062020 user issues fixed

--- mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -85,7 +85,8 @@ namespace mvc.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, "Reporter");
+                    _logger.LogInformation("User created a new reporter account with password.");
 
 
                         await _signInManager.SignInAsync(user, isPersistent: false);
