f1c9b6bb3216177f34bf45d1a6e32304b48ecf32
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2020-06-11 23:14:52 +0200

    11062020 - fixed google registering bug

--- mvc/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ mvc/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -135,21 +135,11 @@ namespace mvc.Areas.Identity.Pages.Account
                     result = await _userManager.AddLoginAsync(user, info);
                     if (result.Succeeded)
                     {
+                        await _userManager.AddToRoleAsync(user, "Reporter");
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
 
-                        //var userId = await _userManager.GetUserIdAsync(user);
-                        //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                        //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
 
-                        //var callbackUrl = Url.Page(
-                        //    "/Account/ConfirmEmail",
-                        //    pageHandler: null,
-                        //    values: new { area = "Identity", userId = userId, code = code },
-                        //    protocol: Request.Scheme);
-
-                        // todo await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                         return LocalRedirect(returnUrl);
                     }
